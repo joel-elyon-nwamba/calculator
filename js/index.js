b4e23395d481af051836a91ab836dd3723bfacf6
Joel Nwamba <joel.elyon.nwamba@gmail.com>
2022-05-09 20:16:47 -0400

    add functions to the buttons

---
+++ js/index.js
@@ -0,0 +1,25 @@
+const numberButtons = document.querySelectorAll(".number");
+const operator = document.querySelectorAll(".operator");
+const deleteOption = document.querySelector(".delete");
+const clear = document.querySelector(".clear");
+const equal = document.querySelector(".equal");
+
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a/b;
+}
+
+function operate() {
+    
+}
