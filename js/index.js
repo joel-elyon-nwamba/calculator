fcee94cc72b24134d26a06f6a749539c498640b0
Joel Nwamba <joel.elyon.nwamba@gmail.com>
2022-05-16 20:58:39 -0400

    Update equal sign

--- js/index.js
+++ js/index.js
@@ -1,8 +1,10 @@
 const numberButtons = document.querySelectorAll(".number");
-const operator = document.querySelectorAll(".operator");
-const deleteOption = document.querySelector(".delete");
+const operatorButtons = document.querySelectorAll(".operator");
 const clear = document.querySelector(".clear");
-const equal = document.querySelector(".equal");
+const deleteOption = document.querySelector(".delete");
+const previousOperand = document.querySelector(".previous-operand");
+const currentOperand = document.querySelector(".current-operand");
+const equal = document.querySelector(".equal")
 
 function add(a, b) {
     return a + b;
@@ -17,9 +19,55 @@ function multiply(a, b) {
 }
 
 function divide(a, b) {
-    return a/b;
+    return a/b
 }
 
-function operate() {
-    
+function operate(num1, num2, operator) {
+    num1 = Number(num1);
+    num2 = Number(num2)
+    if(operator === "+") {
+        return add(num1, num2)
+    } else if(operator === "-") {
+        return subtract(num1, num2)
+    } else if(operator === "*") {
+        return multiply(num1, num2)
+    } else {
+        return divide(num1, num2)
+    }
 }
+
+let storeNumber = "";
+let operatorClicked = "";
+let firstNumber = "";
+let result = "";
+currentOperand.textContent = 0;
+
+numberButtons.forEach(number => {
+    number.addEventListener("click", function() {
+        storeNumber += number.value 
+        previousOperand.textContent += storeNumber;
+    })
+})
+
+operatorButtons.forEach(operator => {
+    operator.addEventListener("click", function() {
+        firstNumber = storeNumber
+        // Get the opertator that is clicked
+        operatorClicked= operator.textContent;
+        previousOperand.textContent = operator + storeNumber;
+        storeNumber = "";
+        console.log(firstNumber + storeNumber);
+        console.log(operatorClicked)
+    })
+})
+
+function calculator() {
+    const result = operate(Number(firstNumber), Number(storeNumber), operatorClicked);
+   currentOperand.textContent = result;
+}
+
+equal.addEventListener("click", calculator);
+
+
+
+
